5d095e8ca3c6009588d44c635fcd358fa2c40aac
JeisonPC <jeida16@yahoo.es>
2023-01-30 19:08:59 -0500

    Agregando cambios de la clase 27

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,15 +1,18 @@
-import {openModal} from './open-modal.js'
 
 export const modalListener = (event) => {
-    
+
     event.preventDefault()
     const img = event.target
-    const link =img.parentElement
+    const link = img.parentElement
+    const videoId = link.dataset.videoid
 
     if (link && link.classList.contains('js-video-link')) {
         // console.log(link.dataset.videoid)
-        
-        openModal(link.dataset.videoid)
+        // lazy loading
+        import(/* webpackChunkName: "modal" */'./open-modal').then(({ openModal }) => {
+
+            openModal(videoId)
+        })
 
     }
 
